e6b34a1385e894937f355f2ebe1885912ccdb455
Whitebamboo <xiunany@andrew.cmu.edu>
2022-09-27 14:34:49 -0400

    upload project

---
+++ Source/TangCityGenerator/TangCityGenerator.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TangCityGenerator.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TangCityGenerator, "TangCityGenerator" );
